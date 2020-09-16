521af489e7cb4319abe4f5aea7b409ef4db5bacf
ryubato <ryubato7@gmail.com>
2020-09-16 16:04:19 +0900

    added bootstrap css and js

--- src/main/java/me/ryubato/web/BoardController.java
+++ src/main/java/me/ryubato/web/BoardController.java
@@ -1,22 +1,27 @@
 package me.ryubato.web;
 
 import lombok.RequiredArgsConstructor;
+import me.ryubato.service.BoardService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
 public class BoardController {
 
-    private final BoardApiController boardApiController;
+    private final BoardService boardService;
 
-    @RequestMapping("/boards")
-    private String getBoards(Model model) {
-        List<BoardListDto> boardListDtos = boardApiController.findAll();
-        model.addAttribute("boards", boardListDtos);
-        return "boards";
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("boards", boardService.getBoards());
+        return "index";
+    }
+
+    @GetMapping("/boards/{id}")
+    public String getBoard(@PathVariable("id") Long id, Model model) {
+        model.addAttribute("board", boardService.getBoard(id));
+        return "boards/detail";
     }
 }

cf03b51e67b7b3051ae35a6a8f667cc66fd8496a
ryubato <ryubato7@gmail.com>
2020-10-16 00:58:39 +0900

    updated

--- src/main/java/me/ryubato/ToyBoardApplication.java
+++ src/main/java/me/ryubato/ToyBoardApplication.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class ToyBoardApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ToyBoardApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ToyBoardApplication.class, args);
+    }
 }

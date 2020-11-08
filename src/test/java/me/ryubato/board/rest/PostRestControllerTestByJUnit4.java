1b562e3265eccf4bcd653915c516f977b15a8d40
ryubato <ryubato7@gmail.com>
2020-11-09 00:04:54 +0900

    Add Controller Unit Test
    Controller Unit Test 추가, MockMvc 한글깨짐 현상 해결

---
+++ src/test/java/me/ryubato/board/rest/PostRestControllerTestByJUnit4.java
@@ -0,0 +1,58 @@
+package me.ryubato.board.rest;
+
+import me.ryubato.board.domain.Post;
+import me.ryubato.board.rest.PostListRspDto;
+import me.ryubato.board.rest.PostRestController;
+import me.ryubato.board.service.PostService;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.Mockito;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+public class PostRestControllerTestByJUnit4 {
+
+    private static MockMvc mockMvc;
+    private static PostService postService;
+
+    @BeforeClass
+    public static void setup() {
+        postService = Mockito.mock(PostService.class);
+        mockMvc = MockMvcBuilders.standaloneSetup(new PostRestController(postService))
+                .addFilter(new CharacterEncodingFilter(StandardCharsets.UTF_8.name()))
+                .build();
+    }
+
+    @Test
+    public void findAll() throws Exception {
+        /* given */
+        Post aPost = Post.builder()
+                .boardId(1L)
+                .id(1L)
+                .title("공지사항")
+                .writer("관리자")
+                .build();
+
+        PostListRspDto postListRspDto = new PostListRspDto(aPost);
+
+        List<PostListRspDto> postListRspDtos = Collections.singletonList(postListRspDto);
+        BDDMockito.given(postService.getPosts()).willReturn(postListRspDtos);
+
+        /* when then */
+        mockMvc.perform(get("/api/v1/posts"))
+                .andExpect(status().isOk());
+    }
+
+}
---
+++ src/test/java/me/ryubato/board/rest/PostRestControllerTestWithWebMvcTest.java
@@ -0,0 +1,72 @@
+package me.ryubato.board.rest;
+
+import me.ryubato.board.domain.Post;
+import me.ryubato.board.service.PostService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcBuilderCustomizer;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@AutoConfigureDataJpa
+@WebMvcTest(PostRestController.class)
+@Import(PostRestControllerTestWithWebMvcTest.MockMvcConfig.class)
+class PostRestControllerTestWithWebMvcTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PostService postService;
+
+    @Test
+    void findAll() throws Exception {
+        /* given */
+        Post aPost = Post.builder()
+                .boardId(1L)
+                .id(1L)
+                .title("공지사항")
+                .content("테스트 입니다.")
+                .build();
+
+        PostListRspDto postListRspDto = new PostListRspDto(aPost);
+        List<PostListRspDto> postListRspDtos = Collections.singletonList(postListRspDto);
+
+        given(postService.getPosts()).willReturn(postListRspDtos);
+
+        /* when */
+        mockMvc.perform(get("/api/v1/posts"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id", equalTo(1)));
+
+    }
+
+    @TestConfiguration
+    static class MockMvcConfig {
+        @Bean
+        MockMvcBuilderCustomizer uft8Config() {
+            return builder -> builder.addFilters(new CharacterEncodingFilter(StandardCharsets.UTF_8.name()))
+                    .alwaysDo(print());
+        }
+    }
+}
